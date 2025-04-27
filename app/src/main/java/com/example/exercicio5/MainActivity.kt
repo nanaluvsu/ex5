f72e050142fcb517f12c8c69b7215bc50a0c9546
nanaluvsu <worksender053@gmail.com>
2025-04-27 19:06:24 -0300

    Updating files - Automated commit!!

--- app/src/main/java/com/example/exercicio5/MainActivity.kt
+++ app/src/main/java/com/example/exercicio5/MainActivity.kt
@@ -22,16 +22,24 @@ class MainActivity : AppCompatActivity() {
             val email = binding.email.text.toString()
             val senha = binding.senha.text.toString()
 
-            if (email == al.email && senha == al.senha) {
-                val toast = Toast.makeText(this, "eba", Toast.LENGTH_SHORT).show()
+            if (email.isBlank()) {
+                val toast = Toast.makeText(this, "Campo e-mail está vazio.", Toast.LENGTH_SHORT).show()
+
+            } else if (email != al.email ) {
+                val toast = Toast.makeText(this, "E-mail não coincide.", Toast.LENGTH_SHORT).show()
+            } else if (senha.isBlank()) {
+                    val toast = Toast.makeText(this, "Campo senha está vazio.", Toast.LENGTH_SHORT).show()
+            } else if (senha != al.senha) {
+                    val toast = Toast.makeText(this, "Senha não coincide. ", Toast.LENGTH_SHORT).show()
+            } else {
+                    val toast = Toast.makeText(this, "eba", Toast.LENGTH_SHORT).show();
                 val intent = Intent(this@MainActivity, login::class.java)
                 startActivity(intent)
-            } else {
-                val toast = Toast.makeText(this, ":c", Toast.LENGTH_SHORT).show()
+                }
             }
         }
         }
-    }
+
 
 class Aluno (
     val email: String,
